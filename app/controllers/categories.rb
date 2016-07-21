f3a69d6f509337d1260e2876d54408be696cfe62
Helom Berhane <email": null>
2016-07-21 15:15:38 -0500

    some styling added

--- app/controllers/categories.rb
+++ app/controllers/categories.rb
@@ -1,3 +1,9 @@
+get '/categories' do
+  @category = Category.find_by(id: params[:id])
+  @user = User.find_by(id: session[:user_id])
+  erb :'categories/show'
+end
+
 get '/categories/:id' do
   @category = Category.find_by(id: params[:id])
   @user = User.find_by(id: session[:user_id])
