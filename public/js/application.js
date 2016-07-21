d453db5fac2330fdbe17a9e6d302f67f59e92e02
Helom Berhane <email": null>
2016-07-21 18:10:43 -0500

    Add Smack Comments with jQuery

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,50 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+  $('#new_comeback_button').on('click', function() {
+
+    $(this).css('display', 'none');
+    $('#new_comeback_form').css('display', 'inline');
+
+  });
+
+
+  $('#new_smack_comment_button').on('click', function() {
+
+    $(this).css('display', 'none');
+    $('#smack_comment_form').css('display', 'inline');
+
+  });
+
+  // $('#new_comeback_form').submit( function(event) {
+
+  //   event.preventDefault();
+  //   var comeback = $("#comeback").val();
+
+  //   if (comeback != "") {
+
+  //     $.ajax({
+  //       url: '/categories/:category_id/smacks/:smack_id/comebacks/new',
+  //       type: 'default GET (Other values: POST)',
+  //       dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
+  //       data: {param1: 'value1'},
+  //     })
+  //     .done(function() {
+  //       console.log("success");
+  //     })
+  //     .fail(function() {
+  //       console.log("error");
+  //     })
+  //     .always(function() {
+  //       console.log("complete");
+  //     });
+
+
+
+
+  //     $("#comeback").val('');
+  //   }
+
+    // $("#comebacks").append('<li class="comment"><article><p>'+comments+'</p><span class="author">'+author_name+'</span></article></li>');
 });
