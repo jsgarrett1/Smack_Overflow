966ba71bdb8216a5a560ba54428618da60c1ee1e
adam baxter <adamscottbaxter@gmail.com>
2016-07-21 21:20:20 -0500

    more css

--- public/js/application.js
+++ public/js/application.js
@@ -35,64 +35,8 @@ $(document).ready(function() {
     .done(function(response) {
       console.log(response);
       console.log(self.parent().find('.points'));
-      self.parent().find('.points').text(response);
+      self.parent().find('.points').text(response + " Points");
     });
   });
 
-  $(".xyz_comeback").css("padding", "20px");
-
-  //   $(".xyz_comeback").on("click", function (event) {
-  //       event.preventDefault();
-
-  //   var self = $(this);
-  //   console.log(self);
-  //   console.log(event);
-  //   var form = $('.form_class')
-  //   var formUrl = $(this).attr('action')
-  //   $(this).css('color', 'red');
-
-  //   $.ajax({
-  //     url: formUrl,
-  //     method: 'post',
-  //     data: {
-  //     }
-  //   })
-  //   .done(function(response) {
-  //     console.log(response);
-  //     console.log(self.parents("#comebacks"));
-  //     // .find('.comeback-points')
-  //     // .text(response);
-  //   });
-  // });
-
-  // $('#new_comeback_form').submit( function(event) {
-
-  //   event.preventDefault();
-  //   var comeback = $("#comeback").val();
-
-  //   if (comeback != "") {
-
-  //     $.ajax({
-  //       url: '/categories/:category_id/smacks/:smack_id/comebacks/new',
-  //       type: 'default GET (Other values: POST)',
-  //       dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
-  //       data: {param1: 'value1'},
-  //     })
-  //     .done(function() {
-  //       console.log("success");
-  //     })
-  //     .fail(function() {
-  //       console.log("error");
-  //     })
-  //     .always(function() {
-  //       console.log("complete");
-  //     });
-
-
-
-
-  //     $("#comeback").val('');
-  //   }
-
-    // $("#comebacks").append('<li class="comment"><article><p>'+comments+'</p><span class="author">'+author_name+'</span></article></li>');
 });
